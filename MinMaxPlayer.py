61ee15306e47b5cc02b424f15061b5f28e02915a
funkey7dan <72495653+funkey7dan@users.noreply.github.com>
2022-04-20 00:32:08 +0300

    Changed score calculation added internal "hashing" function various misc changes

--- MinMaxPlayer.py
+++ MinMaxPlayer.py
@@ -1,16 +1,30 @@
 from Player import Player
 import json
 
+REC_DEPTH = 999
 # its now this players turn to choose a number
 class turn_state:
-
-    def __init__(self,board_state,score_self,score_adv,direction,adv_play = None):
+    hash = None
+    def __init__(self,board_state,score_self,score_adv,direction,agent_type,adv_play = None):
         self.board_state = board_state  #the state of the board - where the ball is on the field
         self.score_self = score_self  # the amount of points left to use
         self.score_adv = score_adv  # the amount of points adversary has to use
         self.adv_play = adv_play  # the number the adversary played
         self.game_state = 0  # -1 lost 1 won 0 neither
         self.plays_now = direction
+        self.agent_type = agent_type
+
+    def get_hash(self):
+        if self.hash:
+            return self.hash
+        hash_value = ''
+        for a in self.__dict__.values():
+            a = str(a)
+            #hash_value += hashlib.md5(a.encode()).hexdigest()
+            hash_value += a+'_'
+        #print(hash_value)
+        self.hash = hash_value
+        return hash_value
 
 def who_won(turn: turn_state):
     if turn.board_state == -3:
@@ -24,107 +38,157 @@ def who_won(turn: turn_state):
             return 1
     return 0
 
+def who_won_alt(turn: turn_state):
+    state = turn.board_state
+    if turn.plays_now == 1:
+        score1 = turn.score_adv
+        score2 = turn.score_self
+    else:
+        score1 = turn.score_self
+        score2 = turn.score_adv
+    if state == -3 or (score1 <= 0 and score2 > 0):
+        return 1
+    # if the ball is in the court of p1
+    # or if player 1 still has points while player 2 not
+    if state == 3 or (score2 <= 0 and score1 > 0):
+        return -1
+    # if both are 0
+    elif score1 <= 0 and score2 <= 0:
+        if state < 0:
+            return 1
+        elif state > 0:
+            return -1
+        else:
+            return 0
+
 class MinMax(Player):
+    # how many recursion levels we allow
     cache = {}
-    def __init__(self,MAX_SCORE):
+    debug_cache = {}
+
+    def __init__(self,MAX_SCORE,load_cache,name = "MinMax"):
         # cache of game states - tuple of turn state and the optimal value to play
-        # try:
-        #     with open("data.json","r") as fp:
-        #         self.cache = json.load(self.cache,fp)
-        # except:
-        #     self.cache = {}
-        # super().__init__("MinMax",MAX_SCORE)
-        super().__init__("MinMax",MAX_SCORE)
+        self.load_cache = load_cache
+        if load_cache:
+            try:
+                with open(name + "data.json","r") as fp:
+                    self.cache = json.load(fp)
+            except Exception as e:
+                self.cache = {}
+        super().__init__(name,MAX_SCORE)
 
     def reset(self,MAX_SCORE):
         super().reset(MAX_SCORE)
 
     # the game evaluation from the position of the minimizing player, returns tuple of smallest value and move
     # leading to it
-    def _min(self,turn: turn_state):
-        if len(self.cache) != 0 and turn in self.cache:
-            print(self.cache[turn])
-            return self.cache[turn]
+    def _min(self,turn: turn_state,depth: int = 0):
+        if depth >= REC_DEPTH:
+            return turn.board_state,-1
+        # set the value of winning for current player
+        WIN_STATE = 3 * turn.plays_now
+        LOSE_STATE = -WIN_STATE
+        if len(self.cache) != 0 and turn.get_hash() in self.cache:
+            #print(self.cache[hash(turn)])
+            self.debug_cache[turn.get_hash()] = turn
+            if turn.score_self >= self.cache[turn.get_hash()][1]:
+                #print("debug")
+                return self.cache[turn.get_hash()]
         min_value = 0  # neither win or loss
         move = -1  # -1 is no move
         # check if the game is won, and who won
         if who_won(turn) == turn.plays_now:
             # we won, return
             move = -1  #not a move, placeholder
-            min_value = 1
+            min_value = WIN_STATE
         elif who_won(turn) == -turn.plays_now:
             move = -1
-            min_value = -1
+            min_value = LOSE_STATE
         else:
-            for i in range(1,turn.score_self + 1):
+            for j in range(1,turn.score_self + 1):
                 # see the board from the view of the opponent, if we played i
                 updated_board = turn.board_state
                 if turn.adv_play is not None:
                     # the number chosen is less than adversary, the one who plays now loses position
-                    if i < turn.adv_play:
+                    if j < turn.adv_play:
                         updated_board -= turn.plays_now
-                    elif i > turn.adv_play:
+                    elif j > turn.adv_play:
                         updated_board += turn.plays_now
                 next_turn = turn_state(board_state = updated_board,score_self = turn.score_adv,
-                                       score_adv = turn.score_self - i,direction = -turn.plays_now,adv_play = i)
-                res,move = self._max(next_turn)
+                                       score_adv = turn.score_self - j,direction = -turn.plays_now,agent_type = "max",
+                                       adv_play = None)
+                res,move = self._max(next_turn,depth = depth + 1)
+
+
                 #finding the minimum
                 if res < min_value or move == -1:
                     min_value = res
-                    move = i
+                    move = j
 
-                    # if it's a loss, its the best outcome for us so we break
-                    if min_value == -1:
-                        self.cache[turn] = (min_value,move)
-                        return min_value,move
+                # if it's a loss, its the lowest outcome for us so we break
+                if min_value == LOSE_STATE:
+                    self.cache[turn.get_hash()] = (min_value,move)
+                    return min_value,move
 
-        self.cache[turn] = (min_value,move)
+        self.cache[turn.get_hash()] = (min_value,move)
         return min_value,move
 
         # the game evaluation from the position of the minimizing player, returns tuple of smallest value and move  #
         # leading to it
 
-    def _max(self,turn: turn_state):
-        if len(self.cache) != 0 and turn in self.cache:
-            print(self.cache[turn])
-            return self.cache[turn]
+    def _max(self,turn: turn_state,depth: int = 0):
+        if depth >= REC_DEPTH:
+            return turn.board_state,-1
+        # set the value of winning for current player
+        WIN_STATE = 3 * turn.plays_now
+        LOSE_STATE = -WIN_STATE
+        if len(self.cache) != 0 and turn.get_hash() in self.cache:
+            #print(self.cache[hash(turn)])
+            self.debug_cache[turn.get_hash()] = turn
+            if turn.score_self >= self.cache[turn.get_hash()][1]:
+                return self.cache[turn.get_hash()]
         max_value = 0  # initialize with neither win or loss
         move = -1  # -1 is no move
         # check if the game is won, and who won
         if who_won(turn) == turn.plays_now:
             # we won, return
             move = -1  #not a move, placeholder
-            max_value = 1
+            max_value = WIN_STATE
         elif who_won(turn) == -turn.plays_now:
             move = -1
-            max_value = -1
+            max_value = LOSE_STATE
         else:
             for i in range(1,turn.score_self + 1):
                 # see the board from the view of the opponent, if we played i
                 updated_board = turn.board_state
+                # if adversary already played
                 if turn.adv_play is not None:
                     # the number chosen is less than adversary, the one who plays now loses position
                     if i < turn.adv_play:
                         updated_board -= turn.plays_now
                     elif i > turn.adv_play:
                         updated_board += turn.plays_now
-                next_turn = turn_state(board_state = updated_board,score_self = turn.score_adv,
+                #pass to the minimizing agent the next state, with the values according to the choice i
+                next_turn = turn_state(board_state = updated_board,score_self = turn.score_adv,agent_type = "min",
                                        score_adv = turn.score_self - i,direction = -turn.plays_now,adv_play = i)
-                res,move = self._min(next_turn) # result of choosing i
-                #finding the maximum
+                res,move = self._min(next_turn,depth = depth + 1)  # result of choosing i
+                #finding the maximum:
+                # if it's a win, its the best outcome for us so we break
+
+                #update the new maximum if larger
                 if res > max_value or move == -1:
                     max_value = res
+                    # the current i is the best move
                     move = i
 
-                    # if it's a win, its the best outcome for us so we break
-                    if max_value == 1:
-                        self.cache[turn] = (max_value,move)
-                        return max_value,move
-
-        self.cache[turn] = (max_value,move)
+                if max_value == WIN_STATE:
+                    self.cache[turn.get_hash()] = (max_value,move)
+                    return max_value,move
+        self.cache[turn.get_hash()] = (max_value,move)
         return max_value,move
 
     def calculate_move(self,turn: turn_state):
+
         outcome,move = self._max(turn)
         return str(max(move,1))
 
@@ -132,11 +196,13 @@ class MinMax(Player):
     def returnValue(self):
         move = self.calculate_move(
             turn = turn_state(board_state = self.game_state,score_self = self.score,score_adv = self.adv_score,
-                              direction = self.direction,adv_play = self.adv_num))
-        self.score -= int(move)
+                              direction = self.direction,adv_play = self.adv_num,agent_type = "max"))
+        print("")
         return move
 
     def saveCache(self):
-        with open("data.json","w") as fp:
-            if (len(self.cache) > 0):
-                json.dump(self.cache,fp)
+        if self.load_cache:
+            with open(self.name+"data.json","w") as fp:
+                if len(self.cache) > 0:
+                    #fp.seek(0)
+                    json.dump(self.cache,fp)
--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import random
 from MinMaxPlayer import MinMax
 from Player import Player
 
-MAX_SCORE = 15
-PLAY_TIMES = 10
-
+MAX_SCORE = 50
+PLAY_TIMES = 10000
 
 score1 = MAX_SCORE
 score2 = MAX_SCORE
@@ -17,8 +16,8 @@ class Human(Player):
         super().__init__(name,MAX_SCORE)
 
     def returnValue(self):
-        self.self_num = input(self.name + " enter a number: ")
-        self.score -= int(self.self_num)
+        self.self_num = input()
+        #self.score -= int(self.self_num)
         return self.self_num
 
 class Random(Player):
@@ -28,15 +27,16 @@ class Random(Player):
 
     def returnValue(self):
         random_num = -1
-        while  random_num > self.score or random_num<0:
+        while random_num > self.score or random_num < 0:
             random_num = random.randint(1,self.score)
         self.self_num = random_num
-        self.score -= self.self_num
+        #self.score -= self.self_num
+        print("")
         return str(self.self_num)
 
 def init():
     global score2,score1
-    print(" Player 1: " + str(score1) + "         Player 2: " + str(score2) + " ")
+    print(p1.name + ": " + str(score1) + "         " + p2.name + ": " + str(score2) + " ")
     print(" --------------------------------- \n"
           " |       |       #       |       | \n"
           " |       |       #       |       | \n"
@@ -47,7 +47,7 @@ def init():
 
 def drawBoard():
     global state
-    print(" Player 1: " + str(score1) + "         Player 2: " + str(score2) + " ")
+    print(p1.name + ": " + str(score1) + "         " + p2.name + ": " + str(score2) + " ")
     print(" --------------------------------- \n"
           " |       |       #       |       | \n"
           " |       |       #       |       | ")
@@ -71,17 +71,20 @@ def drawBoard():
           " --------------------------------- \n")
 
 def check_win_cond(p1,p2):
-    global end,score1,score2,prevState
-
-    if state == 3 or (score1 <= 0 and score2 > 0):
-        print("PLAYER 2 WINS !")
+    global end,score1,score2,prevState,state
+    # if the ball is in the court of p2
+    # or if player 2 still has points while player 1 not
+    if state == -3 or (score1 <= 0 and score2 > 0):
+        print(p2.name + " WINS !")
         print(" --------------------------------- ")
         p1.set_win(False)
         p2.set_win(True)
         end = True
         return
-    if state == -3 or (score2 <= 0 and score1 > 0):
-        print("PLAYER 1 WINS !")
+    # if the ball is in the court of p1
+    # or if player 1 still has points while player 2 not
+    if state == 3 or (score2 <= 0 and score1 > 0):
+        print(p1.name + " WINS !")
         print(" --------------------------------- ")
         p1.set_win(True)
         p2.set_win(False)
@@ -90,14 +93,14 @@ def check_win_cond(p1,p2):
     # if both are 0
     elif score1 <= 0 and score2 <= 0:
         if state < 0:
-            print("PLAYER 2 WINS !")
+            print(p2.name + " WINS !")
             print(" --------------------------------- ")
             p1.set_win(False)
             p2.set_win(True)
             end = True
             return
         elif state > 0:
-            print("PLAYER 1 WINS !")
+            print(p1.name + " WINS !")
             print(" --------------------------------- ")
             p2.set_win(False)
             p1.set_win(True)
@@ -124,28 +127,32 @@ def check_state(num1,num2):
 def playTurn(p1,p2):
     global end,score1,score2,prevState
     while True:
+        print(p1.name + " enter a number: ",end = '')
         num1 = p1.returnValue()
         if num1.isdecimal() and score1 >= int(num1) > 0:
             num1 = int(num1)
             p2.adv_num = num1
             p2.adv_score -= num1
+            p1.score -= num1
             break
-        print("Player1, "+num1+" is NOT A VALID MOVE !")
+        print(p1.name + " " + num1 + " is NOT A VALID MOVE !")
     while True:
+        print(p2.name + " enter a number: ",end = '')
         num2 = p2.returnValue()
         if num2.isdecimal() and score2 >= int(num2) > 0:
             num2 = int(num2)
             p1.adv_num = num2
             p1.adv_score -= num2
+            p2.score -= num2
             break
-        print("Player2, "+num2+" is NOT A VALID MOVE !")
+        print(p2.name + " " + num2 + " is NOT A VALID MOVE !")
     print("\n")
     # pass advesary values to the players
     p1.getPlay(score2,num2,state)
     p2.getPlay(score1,num1,state)
     check_state(num1,num2)
     drawBoard()
-    print("       Player 1 played: " + str(num1) + "\n       Player 2 played: " + str(num2) + "\n\n")
+    print("       " + p1.name + " played: " + str(num1) + "\n       " + p2.name + " played: " + str(num2) + "\n\n")
 
 def playGame(p1,p2):
     global end,score1,score2,prevState
@@ -156,27 +163,33 @@ def playGame(p1,p2):
             break
         playTurn(p1,p2)
 
+
 def battle(p1: Player,p2: Player,rounds: int):
     for i in range(rounds):
-        MAX_SCORE
         gameReset()
         p1.reset(MAX_SCORE)
         p2.reset(MAX_SCORE)
         playGame(p1,p2)
-    print("p1: " + str(p1.wins),"p2: " + str(p2.wins))
-    #if isinstance(p1,MinMax):
-        #p1.saveCache()
+    print(p1.name + ": " + str(p1.wins),p2.name + ": " + str(p2.wins))
+    if isinstance(p1,MinMax):
+        p1.saveCache()
+    if isinstance(p2,MinMax):
+        p2.saveCache()
 
 
 def gameReset():
+
     global state,prevState,score1,score2,end
     prevState = state = 0
     score1 = score2 = MAX_SCORE
     end = False
 
+
 if __name__ == '__main__':
-    p2 = Human("Player 2")
-    p1 = MinMax(MAX_SCORE)
+    #p1 = MinMax(MAX_SCORE,load_cache = False,name = "agent2")
+    p1 = Random("Ran2")
+    #p1 = Human("Meatbag")
+    p2 = MinMax(MAX_SCORE,load_cache = True,name = "agent1")
     p1.direction = -1
     p2.direction = 1
 
